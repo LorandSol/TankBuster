5088e599f11de0382901b18c523136bf0d5d5652
AceofGrades <47581354+LorandSol@users.noreply.github.com>
2022-07-01 13:44:27 +1000

    Made a bouncing bullet script!

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -14,5 +14,16 @@ public class Bullet : MonoBehaviour
     {
         // Bullet goes pew pew (moves forward)
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, Time.deltaTime*speed + .1f, collisionMask))
+        {
+            Vector3 reflect = Vector3.Reflect(ray.direction, hit.normal);
+            float rot = 90 - Mathf.Atan2(reflect.z, reflect.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, rot, 0);
+        }
     }
+
 }
