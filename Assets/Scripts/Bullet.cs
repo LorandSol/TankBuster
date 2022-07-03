a9ef3656b77c1521a41460eccfbcd6ef173a33d2
Layla <layladarwiche02@gmail.com>
2022-07-03 20:05:26 +1000

    Main game level set up

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public List<LayerMask> collisionMask;
+    public LayerMask collisionMask;
 
     public Transform bullet;
     private float speed = 100;
@@ -17,16 +17,15 @@ public class Bullet : MonoBehaviour
 
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
-        foreach (LayerMask layer in collisionMask)
+
+        if (Physics.Raycast(ray, out hit, Time.deltaTime * speed + .1f, collisionMask))
         {
-            if (Physics.Raycast(ray, out hit, Time.deltaTime * speed + .1f, layer))
-            {
-                Vector3 reflect = Vector3.Reflect(ray.direction, hit.normal);
-                float rot = 90 - Mathf.Atan2(reflect.z, reflect.x) * Mathf.Rad2Deg;
-                transform.eulerAngles = new Vector3(0, rot, 0);
-            }
+            Vector3 reflect = Vector3.Reflect(ray.direction, hit.normal);
+            float rot = 90 - Mathf.Atan2(reflect.z, reflect.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, rot, 0);
         }
-      
+
+
 
 
     }
