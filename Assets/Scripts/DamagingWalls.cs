17a2d20ee842125c0f2649c13825720d91f54e43
Layla <layladarwiche02@gmail.com>
2022-07-03 20:45:17 +1000

    Breakable Walls

---
+++ Assets/Scripts/DamagingWalls.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagingWalls : MonoBehaviour
+{
+    public int boxHealth;
+    public GameObject box;
+    public Material damangedMaterial;
+    public Transform damangedBoxes;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player1Bullet") | collision.gameObject.CompareTag("Player2Bullet"))
+        {
+            boxHealth--;
+            GetComponent<Renderer>().material = damangedMaterial; // change material
+            box.transform.SetParent(damangedBoxes); 
+            if(boxHealth == 0)
+            {
+                Destroy(box); 
+            }
+        }
+    }
+}
