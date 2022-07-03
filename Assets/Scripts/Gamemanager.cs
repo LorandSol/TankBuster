f1bf93da570c30c1455a46e1ffb765e7f0d3fc86
AceofGrades <47581354+LorandSol@users.noreply.github.com>
2022-07-03 11:05:42 +1000

    Updated gamemanager and respawn scripts

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -1,30 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using TMPro;
 
 public class Gamemanager : MonoBehaviour
 {
     public int playerScore1;
     public int playerScore2;
+    public TextMeshProUGUI playerScoreText1;
+    public TextMeshProUGUI playerScoreText2;
+    public TextMeshProUGUI timerText;
+
+    public GameObject gameOverPanel;
+    public TextMeshProUGUI player1Win;
+    public TextMeshProUGUI player2Win;
+    public TextMeshProUGUI tieText;
 
     private float timer = 10.0f;
     private bool gameOver = false;
 
-    public Text timerText; 
-
     private void Start()
     {
+        gameOverPanel.SetActive(false);
+        player1Win.gameObject.SetActive(false);
+        player2Win.gameObject.SetActive(false);
+        tieText.gameObject.SetActive(false);
+
+        Time.timeScale = 1;
         playerScore1 = 0;
+        playerScoreText1.text = "Player 1: " + 0;
         playerScore2 = 0;
+        playerScoreText2.text = "Player 2: " + 0;
+        timerText.text = timer.ToString();
 
         InvokeRepeating("Timer", 0.0f, 1.0f);
-
-    }
-
-    private void Update()
-    {
-        DisplayTime(timer);
     }
 
     public void Timer()
@@ -38,30 +47,29 @@ public class Gamemanager : MonoBehaviour
         else
         {
             timer--;
+            timerText.text = timer.ToString();
         }
     }
     
-    public void DisplayTime(float timeToDisplay)
-    {
-        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
-        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds); 
-    }
-
     public void GameOver()
     {
         if (gameOver == true)
         {
+            gameOverPanel.SetActive(true);
+            Time.timeScale = 0;
             if (playerScore1 > playerScore2)
             {
+                player1Win.gameObject.SetActive(true);
                 Debug.Log("Player 1 wins!");
             }
             else if (playerScore2 > playerScore1)
             {
+                player2Win.gameObject.SetActive(true);
                 Debug.Log("Player 2 wins!");
             }
             else if (playerScore1 == playerScore2)
             {
+                tieText.gameObject.SetActive(true);
                 Debug.Log("It's a tie!");
             }
         }
